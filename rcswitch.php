e865b31b2e917b72963ec9071f0f377658c777dc
Jörg Zieren <github@zieren.de>
2023-10-25 10:36:36 +0200

    Track my personal, but maybe generally useful, PHP interface

---
+++ rcswitch.php
@@ -0,0 +1,25 @@
+<?php
+// For Apache to run this, run visudo and add this line:
+// www-data ALL=(ALL) NOPASSWD: /usr/bin/rcswitch
+define('SWITCH_CMD', 'sudo rcswitch');
+date_default_timezone_set('Europe/Berlin');
+
+$systemCode = $_GET['system'];
+$unitCode = $_GET['unit'];
+$state = $_GET['state'];
+
+$message = date('Y-m-d H:i:s').' switching '.$systemCode.':'.$unitCode.' to '.$state.'... ';
+
+$output = array();
+$retval = 0;
+exec(implode(' ', array(SWITCH_CMD, $systemCode, $unitCode, $state, '2>&1')), $output, $retval);
+
+if ($retval === 0) {
+  $message .= "OK\n";
+} else {
+  $message .= 'FAIL: '.implode("\n", $output)."\n";
+}
+$logfile = fopen('/tmp/rcswitch.log', 'a');
+fwrite($logfile, $message);
+fclose($logfile);
+echo $message;
